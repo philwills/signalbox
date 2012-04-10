3c5a13f18b6e15c953c797413f5213a287cbcdec
Philip Wills <philip.wills@guardian.co.uk>
2012-04-10 17:33:33 +0100

    Add listing of repos and fix broken parsing.

--- app/controllers/Github.scala
+++ app/controllers/Github.scala
@@ -1,13 +1,24 @@
 package controllers
 
-import play.api.mvc.Controller
-import play.api.mvc.Action
 import play.api.libs.ws.WS
+import play.api.mvc._
 
 object Github extends Controller {
 
   val clientId = "1e6477b2d1ed022cee4b"
   val secretKey = "xxxxx"
+  
+  def list = Action { implicit request =>
+    withGithubToken { token =>
+      Async {
+        WS.url("https://api.github.com/orgs/%s/repos" format ("guardian"))
+          .withQueryString("access_token" -> token).get(
+        ) map { response =>
+          Ok(views.html.Github.list(response.json \\ "name" map (_.as[String])))
+        }
+      }
+    }
+  }
 
   def authenticate = Action { request =>
     Redirect("https://github.com/login/oauth/authorize", Map("client_id" -> Seq(clientId)))
@@ -22,13 +33,27 @@ object Github extends Controller {
         "client_secret" -> Seq(secretKey),
         "code" -> Seq(code)
       )) map { response =>
-        val results = for {
-          pairs <- response.body.split("&")
-          keyVals <- pairs.split("=")
-        } yield (keyVals(0) -> keyVals(1))
-        val resultMap: Map[String, String] = Map(results.toSeq: _*)
-        Ok(resultMap("access_token"))
+        val resultMap = PairParser.parse(response.body)
+        Redirect(request.session.get("return_uri").getOrElse("/")).
+          withSession("github_token" -> resultMap("access_token"))
       }
     }
+  }
+
+  def withGithubToken(block: String => Result)(implicit request: Request[AnyContent]) = {
+    request.session.get("github_token") map { token =>
+      block(token)
+    } getOrElse {
+      Redirect("/signalbox/authenticate").withSession("return_uri" -> request.uri)
+    }
+  }
+}
+
+object PairParser {
+  def parse(pairString: String) = {
+    Map(pairString.split("&") map { x =>
+      val pairs = x.split("=")
+      (pairs(0) -> pairs(1))
+    } : _*)
   }
 }
---
+++ test/PairParsingSpec.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class PairParsingSpec extends Specification {
+  "The pair parser" should {
+    "extract a map" in {
+      val map = PairParser.parse("a=b&c=d")
+      map("a") must be equalTo("b")
+    }
+  }
+}
